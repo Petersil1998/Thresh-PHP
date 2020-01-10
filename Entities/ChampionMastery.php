62f5b1f60b5d2d5ad48c103e9f28619d83c5abab
Petersil1998 <silbernagl.peter@rolmail.net>
2020-01-10 19:35:52 +0100

    Added Championmastery Entity

---
+++ Entities/ChampionMastery.php
@@ -0,0 +1,139 @@
+<?php
+
+
+class ChampionMastery
+{
+    /**
+     * @var bool
+     */
+    private $chestGranted;
+
+    /**
+     * @var int
+     */
+    private $championLevel;
+
+    /**
+     * @var int
+     */
+    private $championPoints;
+
+    /**
+     * @var int
+     */
+    private $championId;
+
+    /**
+     * @var int
+     */
+    private $championPointsUntilNextLevel;
+
+    /**
+     * @var int
+     */
+    private $lastPlayTime;
+
+    /**
+     * @var int
+     */
+    private $tokensEarned;
+
+    /**
+     * @var int
+     */
+    private $championPointsSinceLastLevel;
+
+    /**
+     * @var string
+     */
+    private $summonerId;
+
+    /**
+     * ChampionMastery constructor.
+     * @param $championMastery mixed
+     */
+    public function __construct($championMastery)
+    {
+        $this->chestGranted = $championMastery->chestGranted;
+        $this->championLevel = $championMastery->championLevel;
+        $this->championPoints = $championMastery->championPoints;
+        $this->championId = $championMastery->championId;
+        $this->championPointsUntilNextLevel = $championMastery->championPointsUntilNextLevel;
+        $this->lastPlayTime = $championMastery->lastPlayTime;
+        $this->tokensEarned = $championMastery->tokensEarned;
+        $this->championPointsSinceLastLevel = $championMastery->championPointsSinceLastLevel;
+        $this->summonerId = $championMastery->summonerId;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isChestGranted()
+    {
+        return $this->chestGranted;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChampionLevel()
+    {
+        return $this->championLevel;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChampionPoints()
+    {
+        return $this->championPoints;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChampionId()
+    {
+        return $this->championId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChampionPointsUntilNextLevel()
+    {
+        return $this->championPointsUntilNextLevel;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastPlayTime()
+    {
+        return $this->lastPlayTime;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTokensEarned()
+    {
+        return $this->tokensEarned;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChampionPointsSinceLastLevel()
+    {
+        return $this->championPointsSinceLastLevel;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSummonerId()
+    {
+        return $this->summonerId;
+    }
+}
