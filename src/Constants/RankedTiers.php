22a8e91eceb6792c038e1411594e024ba375920d
Petersil1998 <silbernagl.peter@rolmail.net>
2020-08-31 20:32:57 +0200

    Added Ranked Tier enum

---
+++ src/Constants/RankedTiers.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Thresh\Constants;
+
+
+class RankedTiers
+{
+    const IRON = 'IRON';
+    const BRONZE = 'BRONZE';
+    const SILVER = 'SILVER';
+    const GOLD = 'GOLD';
+    const PLATINUM = 'PLATINUM';
+    const DIAMOND = 'DIAMOND';
+}
