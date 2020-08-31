ada95667b2128fb487342037fd50248ef68c957b
Petersil1998 <silbernagl.peter@rolmail.net>
2020-08-31 21:38:26 +0200

    Added the posibility to use filters to every endpoint

--- src/Helper/HTTPClient.php
+++ src/Helper/HTTPClient.php
@@ -46,7 +46,7 @@ class HTTPClient
      * @param $extraData array
      * @return string
      */
-    private function request($url, $game, $extraData = array()){
+    private function request($url, $game, $extraData){
         $basePath = '';
         switch ($game){
             case 'lol':
@@ -107,46 +107,51 @@ class HTTPClient
     /**
      * Sends a request to the Summoner Endpoint
      * @param $url string
+     * @param array $filter
      * @return string
      */
-    public function requestSummonerEndpoint($url){
-        return $this->request('summoner/v4/'.$url, 'lol');
+    public function requestSummonerEndpoint($url, $filter = array()){
+        return $this->request('summoner/v4/'.$url, 'lol', $filter);
     }
 
     /**
      * Sends a request to the Champion Mastery Endpoint
      * @param $url string
+     * @param array $filter
      * @return string
      */
-    public function requestChampionMasteryEndpoint($url){
-        return $this->request('champion-mastery/v4/'.$url, 'lol');
+    public function requestChampionMasteryEndpoint($url, $filter = array()){
+        return $this->request('champion-mastery/v4/'.$url, 'lol', $filter);
     }
 
     /**
      * Sends a request to the League Endpoint
      * @param $url string
+     * @param array $filter
      * @return string
      */
-    public function requestLeagueEndpoint($url){
-        return $this->request('league/v4/'.$url, 'lol');
+    public function requestLeagueEndpoint($url, $filter = array()){
+        return $this->request('league/v4/'.$url, 'lol', $filter);
     }
 
     /**
      * Sends a request to the TFT League Endpoint
      * @param $url string
+     * @param array $filter
      * @return string
      */
-    public function requestTftLeagueEndpoint($url){
-        return $this->request('league/v1/'.$url, 'tft');
+    public function requestTftLeagueEndpoint($url, $filter = array()){
+        return $this->request('league/v1/'.$url, 'tft', $filter);
     }
 
     /**
      * Sends a request to the Spectator Endpoint
      * @param $url string
+     * @param array $filter
      * @return string
      */
-    public function requestSpectatorEndpoint($url){
-        return $this->request('spectator/v4/'.$url, 'lol');
+    public function requestSpectatorEndpoint($url, $filter = array()){
+        return $this->request('spectator/v4/'.$url, 'lol', $filter);
     }
 
     /**
@@ -162,9 +167,10 @@ class HTTPClient
     /**
      * Sends a request to the Account Endpoint
      * @param $url string
+     * @param array $filter
      * @return string
      */
-    public function requestAccountEndpoint($url){
-        return $this->request('account/v1/'.$url, 'riot');
+    public function requestAccountEndpoint($url, $filter = array()){
+        return $this->request('account/v1/'.$url, 'riot', $filter);
     }
 }
